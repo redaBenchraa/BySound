359af37c178e3a551a497f637170ee6e9c199231
Reda BENCHRAA <medRedaBENCHRAA@gmail.com>
2016-11-11 17:39:18 +0000

    Timer fully functionning

--- js/script.js
+++ js/script.js
@@ -115,8 +115,8 @@ $(function(){
       var timer = $(".timer input").val();
       if($.isNumeric(timer)){
         clearTimeout(timerVar);
-        timerVar = setTimeout(loadTimer, timer*60*1000);
         $("#clockdiv").fadeIn();
+        initializeClock('clockdiv', new Date(Date.parse(new Date()) + timer * 60 * 1000));
       }else{
         alertify.message('Please enter a numerical value');
         }
@@ -307,4 +307,42 @@ function loadTimer(){
     timeout: 5000
     });
     $(".timer input").val("");
+    $("#clockdiv").fadeOut();
+}
+
+
+
+function getTimeRemaining(endtime) {
+  var t = Date.parse(endtime) - Date.parse(new Date());
+  var seconds = Math.floor((t / 1000) % 60);
+  var minutes = Math.floor((t / 1000 / 60) % 60);
+  var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+  var days = Math.floor(t / (1000 * 60 * 60 * 24));
+  minutes = minutes + hours*60 + days*24*60;
+  return {
+    'total': t,
+    'days': days,
+    'hours': hours,
+    'minutes': minutes,
+    'seconds': seconds
+  };
+}
+
+function initializeClock(id, endtime) {
+  var clock = document.getElementById(id);
+  var minutesSpan = clock.querySelector('.minutes');
+  var secondsSpan = clock.querySelector('.seconds');
+  function updateClock() {
+    var t = getTimeRemaining(endtime);
+    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+    if (t.total <= 0) {
+      loadTimer();
+      clearInterval(timerVar);
+    }
+  }
+
+  updateClock();
+  timerVar = setInterval(updateClock, 1000);
 }
